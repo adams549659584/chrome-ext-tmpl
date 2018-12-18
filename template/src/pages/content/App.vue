728eac98882805e26c89534f4781125b222a3388
罗君 <13760614423@163.com>
2018-12-18 15:57:57 +0800

    不要id 误导

--- template/src/pages/content/App.vue
+++ template/src/pages/content/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     content
   </div>
 </template>
--- template/src/pages/popup/App.vue
+++ template/src/pages/popup/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     popup
     <hello-world></hello-world>
   </div>
